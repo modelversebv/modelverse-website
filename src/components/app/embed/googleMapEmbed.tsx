db02690cc40f081d6f8f2cda526aca76eae77ef6
mateiAvram <matei.avram01@gmail.com>
2025-11-10 15:25:07 +0100

    (fix): Layout issues

--- src/components/app/embed/googleMapEmbed.tsx
+++ src/components/app/embed/googleMapEmbed.tsx
@@ -60,8 +60,10 @@ export function GoogleMapEmbed() {
               d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126ZM12 15.75h.007v.008H12v-.008Z"
             />
           </svg>
-          <h1 className="text-lg">GoogleMaps content blocked</h1>
-          <p className="text-center text-sm text-black/70">
+          <h1 className="text-center text-base md:text-lg 2xl:text-2xl">
+            GoogleMaps content blocked
+          </h1>
+          <p className="text-center text-xs text-black/70 md:text-sm 2xl:text-lg">
             This content is provided by GoogleMaps and requires your consent
           </p>
           <button
--- src/components/app/embed/youtubeEmbed.tsx
+++ src/components/app/embed/youtubeEmbed.tsx
@@ -69,8 +69,10 @@ export function YouTubeEmbed({ videoId, title, blog }: YouTubeEmbedProps) {
                 d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126ZM12 15.75h.007v.008H12v-.008Z"
               />
             </svg>
-            <h1 className="text-lg">YouTube content blocked</h1>
-            <p className="text-center text-sm text-black/70">
+            <h1 className="text-center text-base md:text-lg lg:text-xl 2xl:text-2xl">
+              YouTube content blocked
+            </h1>
+            <p className="text-center text-xs text-black/70 md:text-sm lg:text-base 2xl:text-lg">
               This content is provided by YouTube and requires your consent
             </p>
             <button
