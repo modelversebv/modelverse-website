f7b204e4706a5be97a518b7063e995723f22cf11
mateiAvram <matei.avram01@gmail.com>
2026-01-16 13:10:42 +0100

    (fix): removed div around navlinks

--- src/components/app/navigation/navlink.tsx
+++ src/components/app/navigation/navlink.tsx
@@ -8,17 +8,15 @@ type NavLinkProps = {
 
 export function NavLink({ active = false, to, children }: NavLinkProps) {
   return (
-    <div className="group relative">
-      <Link
-        to={to}
-        className={`rounded-full font-semibold text-black lg:font-normal`}
+    <Link
+      to={to}
+      className={`rounded-full font-semibold text-black lg:font-normal`}
+    >
+      <div
+        className={`flex flex-row gap-2 rounded-full px-4 py-2 transition-all duration-300 ${active ? 'bg-linear-to-r from-green-500/30 to-teal-500/30' : 'opacity-90 lg:opacity-70 lg:hover:opacity-100'}`}
       >
-        <div
-          className={`flex flex-row gap-2 rounded-full px-4 py-2 transition-all duration-300 ${active ? 'bg-linear-to-r from-green-500/30 to-teal-500/30' : 'opacity-90 lg:opacity-70 lg:hover:opacity-100'}`}
-        >
-          {children}
-        </div>
-      </Link>
-    </div>
+        {children}
+      </div>
+    </Link>
   )
 }
